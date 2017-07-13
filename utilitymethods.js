659a7317a48270b95896efdea0c05c07784c08f6
Vikas Bajaj <vikasbajajs@gmail.com>
2017-07-13 19:24:54 +0530

    Add files via upload

---
+++ utilitymethods.js
@@ -0,0 +1,16 @@
+function parseLocalDate(date) {
+    /**
+     * Construct a date object in the local timezone by parsing the input date string, assuming a YYYY-MM-DD format.
+     * Note that the Date(dateString) constructor is explicitly avoided as it may implicitly assume a UTC timezone.
+     */
+    const dateComponents = date.split(/\-/);
+    return new Date(dateComponents[0], dateComponents[1] - 1, dateComponents[2]);
+}
+
+function isValidDate(date) {
+    try {
+        return !(isNaN(parseLocalDate(date).getTime()));
+    } catch (err) {
+        return false;
+    }
+}
