9cde8ddc65ce20ebb4254ee1216bdcdfb31fe738
ratewar <rajatratewal@gmail.com>
2017-07-16 15:02:39 +0530

    Integrated s3 event call

--- uploadimagetrigger/imageUploaded.js
+++ uploadimagetrigger/imageUploaded.js
@@ -1,15 +1,18 @@
 'use strict';
 
-/*var AWS = require('aws-sdk');
+/*
+work required to store values in DynamoDB
+var AWS = require('aws-sdk');
 var docClient = new AWS.DynamoDB.DocumentClient();
 var table = "bidimagemaster";*/
 
-module.exports.s3UploadTriggered = (event, context, callback) => {
 
+module.exports = function (event) {
+  
   var bucket = event.Records[0].s3.bucket.name;
   var filename = event.Records[0].s3.object.key;
-  var UniqueRef = filename.substring(0,filename.indexOf("%24"));
+  var UniqueRef = filename.substring(0,filename.indexOf("%24")); //%24 is $ sign and when filename has special characters its passed as UTF8
   const response = {message: 'A new file Named' + filename + ' was uploaded to the bucket ' + bucket + ' with unique ref ' + UniqueRef,event};
   console.log('A new file named:'+filename +' was created' + ' and the unique ref is ' + UniqueRef);
   callback(null, response);
-};
+}
