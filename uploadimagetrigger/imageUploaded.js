8f83a47dbe6ba44ff2e8cf2a3923b55c055c8357
Vikas Bajaj <vikasbajajs@gmail.com>
2017-07-17 14:45:23 +0530

    Updated the ImageUploadTrigger

--- uploadimagetrigger/imageUploaded.js
+++ uploadimagetrigger/imageUploaded.js
@@ -1,10 +1,9 @@
 'use strict';
 
-/*
-work required to store values in DynamoDB
 var AWS = require('aws-sdk');
+var uuid = require('node-uuid');
 var docClient = new AWS.DynamoDB.DocumentClient();
-var table = "bidimagemaster";*/
+var table = "bidimagemaster";
 
 
 module.exports = function (event) {
@@ -14,4 +13,30 @@ module.exports = function (event) {
   var UniqueRef = filename.substring(0,filename.indexOf("%24")); //%24 is $ sign and when filename has special characters its passed as UTF8
   const response = {message: 'A new file Named' + filename + ' was uploaded to the bucket ' + bucket + ' with unique ref ' + UniqueRef,event};
   console.log('A new file named:'+filename +' was created' + ' and the unique ref is ' + UniqueRef);
+
+
+  var uuid1 = uuid.v1();
+
+  var params = {
+    TableName:table,
+    Item:{
+        "unique_id": uuid1,
+        "bid_ref": UniqueRef,
+        "filename":'https://s3.amazonaws.com/'+ bucket + '/' + filename
+    }
+  };
+
+  console.log(uuid1);
+  console.log(UniqueRef);
+  console.log(filename);
+
+  console.log("Adding a new item...");
+  docClient.put(params, function(err, data) {
+    if (err) {
+        console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Added item:", JSON.stringify(data, null, 2));
+    }
+  });
+
 }
